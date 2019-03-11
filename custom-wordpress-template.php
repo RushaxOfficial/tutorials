8547bb32fc3f2b8035904a018103415d704cc99e
Rushax | Web Design, Digital Marketing & Graphic Design <info@rushax.com>
2019-03-11 21:34:40 +0000

    Update custom-wordpress-template.php

--- custom-wordpress-template.php
+++ custom-wordpress-template.php
@@ -6,15 +6,19 @@ get_header();?>
 
 <div class="container">
   <div class="row">
+    <!--START TITLE CONTAINER -->
     <div class="col">
       <h1><?php the_title();?></h1>
     </div>
+    <!--/END TITLE CONTAINER-->
   </div>
   
   <div class="row">
+    <!--START CONTENT-->
     <div class="col">
       <?php the_content(); ?>
     </div>
+    <!--/END CONTENT-->
   </div>
 </div>
 
